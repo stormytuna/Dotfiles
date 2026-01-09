ce844676d3f1053c17f572676afba0f583652edc
stormytuna <stormytuna@outlook.com>
2026-01-09 08:41:40 +0000

    change wallpaper, nvim stuffs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,7 +3,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- LSPs
-vim.lsp.enable({ 'lua_ls', 'nil_ls', 'roslyn_ls' })
+vim.lsp.enable({ 'lua_ls', 'nil_ls', 'roslyn_ls', 'jdtls' })
 
 vim.lsp.config['lua_ls'] = {
   settings = {
@@ -30,6 +30,7 @@ require('config.lazy')
 
 -- Options
 vim.o.number = true
+vim.o.relativenumber = true
 vim.o.mouse = 'a'
 vim.o.tabstop = 4
 vim.o.shiftwidth = 2
@@ -37,12 +38,21 @@ vim.o.clipboard = 'unnamedplus'
 vim.o.signcolumn = 'yes'
 vim.o.swapfile = false
 vim.o.winborder = 'single'
+vim.o.scrolloff = 10
 
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
 -- Diagnostics
-vim.diagnostic.config({ virtual_lines = true })
+vim.diagnostic.config({
+  virtual_lines = true,
+  virutal_text = true,
+  underline = {
+    severity = {
+      min = vim.diagnostic.severity.WARN,
+    },
+  },
+})
 
 -- Keymaps
 vim.keymap.set('n', '<esc>', '<esc>:nohlsearch<cr>:helpclose<cr>')
@@ -51,9 +61,13 @@ vim.keymap.set('n', '<leader>o', ':update<cr>:source<cr>')
 vim.keymap.set('n', '<leader>w', ':write<cr>')
 vim.keymap.set('n', '<leader>q', ':quit<cr>')
 vim.keymap.set('n', '<leader>e', '<cmd>Oil --float<cr>')
+vim.keymap.set('n', '<c-d>', '<c-d>zz')
+vim.keymap.set('n', '<c-u>', '<c-u>zz')
 
 vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
 vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
+vim.keymap.set('n', '<leader>fd', '<cmd>Telescope diagnostics<cr>')
+vim.keymap.set('n', '<leader>fr', '<cmd>Telescope resume<cr>')
 
 vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename)
 vim.keymap.set('n', '<leader>lr', vim.lsp.buf.references)
@@ -62,6 +76,7 @@ vim.keymap.set('n', '<leader>ld', vim.lsp.buf.type_definition)
 vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action)
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
 vim.keymap.set('i', '<c-s-space>', vim.lsp.buf.signature_help)
+vim.keymap.set('i', '<c-bs>', '<c-w>')
 
 -- Autocommands
 vim.api.nvim_create_autocmd('TextYankPost', {
--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -12,11 +12,40 @@ return {
     appearance = {
       nerd_font_variant = 'mono'
     },
-    completion = { documentation = { auto_show = true } },
+    completion = {
+      documentation = {
+        auto_show = true
+      },
+      menu = {
+        draw = {
+          components = {
+            kind_icon = {
+              text = function(ctx)
+                local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
+                return kind_icon
+              end,
+              highlight = function(ctx)
+                local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+                return hl
+              end,
+            },
+            kind = {
+              highlight = function(ctx)
+                local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+                return hl
+              end,
+            },
+          },
+          columns = { { 'kind_icon', 'kind', gap = 1 }, { 'label', 'label_description', gap = 1 } },
+          treesitter = { 'lsp' }
+        }
+      }
+    },
     sources = {
       default = { 'lsp', 'path', 'snippets', 'buffer' },
     },
-    fuzzy = { implementation = "prefer_rust_with_warning" }
+    fuzzy = { implementation = "prefer_rust_with_warning" },
+    signature = { enabled = true },
   },
   opts_extend = { "sources.default" }
 }
